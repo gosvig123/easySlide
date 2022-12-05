6a2fb1362c8ee83441771cf080a003da66a8d18f
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2022-12-05 18:39:39 +0100

    deallocate files to server and update function file

---
+++ server/lib/openAiFunctions.ts
@@ -0,0 +1,77 @@
+/** @format */
+
+// this function calls the Text API from Open and returns the queried text
+// it takes 3 arguments
+// 1: the search query for text
+// 2: the number of characters to return
+// 3: the API key (only the key value)
+
+export const openAiText = async (
+  searchQuery: string,
+  textLength: number,
+  apiKey: string
+) => {
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/engines/davinci/completions",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+          organization: "Qf25exKC4RF5oBGT5JyaHIw2",
+        },
+        body: JSON.stringify({
+          prompt: searchQuery,
+          max_tokens: textLength,
+          temperature: 0.7,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0.6,
+        }),
+      }
+    ).then((response) => response.json());
+
+    return response.choices[0].text;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// this function calls the Open Ai Image API and returns the image URL
+// it takes 4 arguments
+// 1: the search query for an image
+// 2: the number of images to return
+// 3: the size of the image
+// 4: the API key (only the key value)
+
+export const getImageFromOpenAi = async (
+  searchQuery: string,
+  numberOfImages: number,
+  sizeOfImage: string,
+  apiKey: string
+) => {
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+          organization: "Qf25exKC4RF5oBGT5JyaHIw2",
+        },
+        body: JSON.stringify({
+          prompt: searchQuery,
+          n: numberOfImages,
+          size: sizeOfImage,
+        }),
+      }
+    ).then((response) => response.json());
+    return response.data[0].url as string;
+  } catch (error) {
+    error.log(error);
+  }
+};
