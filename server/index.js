65a1fd373cdc5afc156f5650ecce63c72428870e
eduim <eduardo.im92@gmail.com>
2022-12-06 10:35:11 +0100

    fixed: port client and server no conflicts

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.server_port || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello Abhi!");
