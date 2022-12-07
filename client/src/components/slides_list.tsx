5a77c9f92c2831306c03826f66b45c96f715456e
Ege Egdirici <egd3g3@gmail.com>
2022-12-07 17:28:25 +0100

    style: indentation and line breaks fixed

--- client/src/components/slides_list.tsx
+++ client/src/components/slides_list.tsx
@@ -7,14 +7,14 @@ import { PhoneIcon } from "@chakra-ui/icons";
 
 
 export default function SlidesList() {
-    // create a slides class
+  // create a slides class
 
-    // create a slide object containing id, title, description, image, and link
+  // create a slide object containing id, title, description, image, and link
 
-    // create use state for slides
+  // create use state for slides
   const [slides, setSlides] = useState<any[]>([]);
 
-    // define type of an empty array use state example
+  // define type of an empty array use state example
 
   const addSlide = () => {
     setSlides([...slides, 1]);
@@ -31,10 +31,7 @@ export default function SlidesList() {
       bg="red"
       w="250"
       h="100vh"
-      align={"center"}
-
-
-    >
+      align={"center"}>
       <Center mt={10} w="130px" flexShrink="0" h="100px" bg="tomato" color="white" borderWidth="1px"
         borderColor="white" borderRadius="12px" mb="10px">
         <PhoneIcon />
@@ -42,12 +39,8 @@ export default function SlidesList() {
       {slides.length > 0 &&
         slides.map((slide, index) => (
           <Box textAlign="center" style={{ flexShrink: '0', marginBottom: "15px", marginTop: "15px", borderStyle: "solid", borderRadius: "12px", position: "relative", borderColor: "white", borderWidth: "1px", width: 130, height: 100, color: "white" }}
-
             key={index}>
-
-
             {slide}
-
           </Box>
         ))}
       <Center mt="20px" borderRadius="12px" mr="15px" ml="15px" w="150px" h="100" bg="tomato" color="white">
