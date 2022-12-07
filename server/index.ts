585252b49b79e1ffe8187bd449a08bfffa404fa8
Manel Soler <manelsoler@gmail.com>
2022-12-07 15:57:14 +0100

    imported router to index file

--- server/index.ts
+++ server/index.ts
@@ -1,45 +1,37 @@
-/** @format */
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import router from './router';
+import cors from 'cors';
 
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import { getImageFromOpenAi, openAiText } from "./lib/open-ai-functions";
-const router = express.Router();
-const PORT = process.env.PORT;
 
-const app: Express = express();
-app.use(router);
-const port = process.env.PORT;
-app.use(express.json());
+import { getImageFromOpenAi } from "./lib/open-ai-functions";
+
 
 dotenv.config();
+const openAIkey: string | undefined = process.env.API_KEY
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Abhi!");
-});
+if (!openAIkey) {
+  throw new Error("No API key found");
+}
+
+
+const app: Express = express();
+const PORT = process.env.server_port;
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
 
 app.post("/openimage", async (req: Request, res: Response) => {
   console.log(req.body);
   const { prompt, n, size } = req.body;
-  const image = await getImageFromOpenAi(prompt, n, size, process.env.API_KEY);
+  const image = await getImageFromOpenAi(prompt, n, size, openAIkey);
   console.log(image);
   res.json(image);
 });
 
-
-app.post("/opentext", async (req: Request, res: Response) => {
-  console.log(req.body);
-  const { searchQuery, textLength } = req.body;
-  const text = await openAiText(
-    searchQuery,
-    textLength,
-    process.env.API_KEY
-  );
-  console.log(text);
-  res.json(text);
-});
-
-app.listen(8080 || process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+app.listen(8080 || PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
 
 export default app;
---
+++ server/router.ts
@@ -0,0 +1,17 @@
+
+import express, { Request, Response } from 'express';
+const router = express.Router();
+
+// create a presentation post request]
+
+router.post('/presentation', (req: Request, res: Response) => {
+
+
+
+
+
+});
+
+
+
+export default router;
