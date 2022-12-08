2cb4574149956624f30eeb85f918b65c9d63c601
eduim <eduardo.im92@gmail.com>
2022-12-08 17:30:20 +0100

    test: serverstarter added

--- server/__tests__/api.test.ts
+++ server/__tests__/api.test.ts
@@ -1,4 +1,24 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test, beforeAll, afterAll } from '@jest/globals';
+import { Server } from 'http';
+import axios, { AxiosInstance } from "axios";
+import startServer from '../app'
+
+let server: Server
+let api: AxiosInstance
+
+beforeAll(async () => {
+  server = await startServer()
+  const addressInfo = server.address()
+  if (typeof addressInfo === "string" || addressInfo === null) {
+    console.error(
+      "Port is null. This is possibly because the server is not running."
+    );
+    process.exit(1);
+  }
+  api = axios.create({
+    baseURL: `http://localhost:${addressInfo.port}`,
+  });
+})
 
 
 describe('first test', () => {
