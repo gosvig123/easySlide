749986404a44fdc5cb5fcfa6c22a755dd46b2adb
Arol <arol.vinolas@gmail.com>
2022-12-09 17:49:43 +0100

    test(server): increment timout to 60s

--- server/__tests__/api.test.ts
+++ server/__tests__/api.test.ts
@@ -14,6 +14,8 @@ import prisma from "../lib/prisma";
 let server: Server;
 let api: AxiosInstance;
 
+jest.setTimeout(60000);
+
 beforeAll(async () => {
   server = await startServer();
   const addressInfo = server.address();
