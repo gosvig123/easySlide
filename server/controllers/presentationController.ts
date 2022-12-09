6f5a15bd49b590e0535c423f755c94949083e0c2
eduim <eduardo.im92@gmail.com>
2022-12-09 13:15:36 +0100

    fixed: added changes of main

--- server/controllers/presentationController.ts
+++ server/controllers/presentationController.ts
@@ -1,9 +1,11 @@
-import { Presentation } from '@prisma/client';
-import { Request, Response } from 'express';
-import { createPresentation, getAllPresentations, getPresentationById } from '../models/presentationModel';
-import { createSlide } from '../models/slidesModel';
-
-
+import { Presentation } from "@prisma/client";
+import { Request, Response } from "express";
+import {
+  createPresentation,
+  getAllPresentations,
+  getPresentationById,
+} from "../models/presentationModel";
+import { createSlide } from "../models/slidesModel";
 
 const PresentationController = {
   async createPresentation(req: Request, res: Response) {
@@ -11,8 +13,8 @@ const PresentationController = {
     const body = req.body;
 
     if (body) {
-      const presentation = await createPresentation(body)
-      return res.status(201).json(presentation)
+      const presentation = await createPresentation(body);
+      return res.status(201).json(presentation);
     } else {
       return res.status(400).send("error no body");
     }
@@ -23,8 +25,8 @@ const PresentationController = {
     // else return error
 
     if (req.body) {
-      const presentations = await getAllPresentations()
-      return res.status(200).json(presentations)
+      const presentations = await getAllPresentations();
+      return res.status(200).json(presentations);
     } else {
       return res.status(400).send("error no body");
     }
@@ -34,27 +36,23 @@ const PresentationController = {
     const id = parseInt(req.params.id);
 
     if (id) {
-
-      const presentation: Presentation | null = await getPresentationById(id)
-      return res.status(200).json(presentation)
+      const presentation: Presentation | null = await getPresentationById(id);
+      return res.status(200).json(presentation);
     } else {
       return res.status(400).send("error no body");
     }
   },
-};
 
   async createSlide(req: Request, res: Response) {
-    const id = parseInt(req.params.id)
+    const id = parseInt(req.params.id);
 
     if (id) {
-      const slide = await createSlide(id, req.body)
-      return res.status(201).json(slide)
+      const slide = await createSlide(id, req.body);
+      return res.status(201).json(slide);
     } else {
-      return res.status(400).send("error no id")
+      return res.status(400).send("error no id");
     }
+  },
+};
 
-  }
-}
-
-export default PresentationController
-
+export default PresentationController;
