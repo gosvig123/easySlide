60f5e0f59cd77254921e136fd32292a3552ed239
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2022-12-31 18:49:49 +0100

    added text editing

--- client/src/components/SlidePage.tsx
+++ client/src/components/SlidePage.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { Container, Text } from "@chakra-ui/react";
+import { Container, Text, Textarea } from "@chakra-ui/react";
 import React from "react";
 
 interface Slide {
@@ -17,6 +17,11 @@ export default function SlidePage(props: SlidePageProps) {
   const { slide } = props;
   const { text } = slide || "";
   const { image } = slide || "";
+  const [textValue, setTextValue] = React.useState(text || "");
+
+  // TODO 1: manage text state for new text and existing text
+
+  // TODO 2: store data on server onSlideChange
 
   return (
     <Container
@@ -52,6 +57,14 @@ export default function SlidePage(props: SlidePageProps) {
         bgRepeat="no-repeat"
         bgPosition="right"
       >
+        <Textarea
+          fontSize={"2xl"}
+          h={"100%"}
+          w={"100%"}
+          onChange={(e) => setTextValue(e.target.value)}
+          value={textValue}
+          border={0}
+        ></Textarea>
         <Text>{text}</Text>
       </Container>
     </Container>
